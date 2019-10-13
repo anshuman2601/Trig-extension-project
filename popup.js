a52d7fa2b21b176e8d36126f67667c659ec108c3
Anshuman <anshuman2601@gmail.com>
2019-10-12 19:32:35 -0500

    Revert "GCP Initialization"
    This reverts commit 661621ba88df362edbee53a24740131d2ad59adf.

--- popup.js
+++ popup.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
         d = document;
   
         var f = d.createElement('form');
-        f.action = 'http://drive.google.com';
+        f.action = 'http://gtmetrix.com/analyze.html?bm';
         f.method = 'post';
         var i = d.createElement('input');
         i.type = 'hidden';
